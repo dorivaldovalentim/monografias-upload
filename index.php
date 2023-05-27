ab7ef4ae75057ae98e69bcb260d3f6ad8b1e4fc6
Dorivaldo Valentim <dorivaldovalentim@gmail.com>
2023-05-27 17:12:17 +0100

    Adicionei um pacote para dump e outro para apresentação de erros

--- index.php
+++ index.php
@@ -2,6 +2,10 @@
 
 require_once __DIR__ . "./vendor/autoload.php";
 
+$whoops = new \Whoops\Run;
+$whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
+$whoops->register();
+
 session_start();
 
 use App\Database\Database;
