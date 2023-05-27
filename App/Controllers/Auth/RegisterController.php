69fc53a97555b6619dfd60968e6c1c7979798464
Dorivaldo Valentim <dorivaldovalentim@gmail.com>
2023-05-27 16:14:10 +0100

    Terminei a funcionalidade de registrar usuário, por enquanto

--- App/Controllers/Auth/RegisterController.php
+++ App/Controllers/Auth/RegisterController.php
@@ -7,42 +7,16 @@ use App\Controllers\Controller;
 
 class RegisterController extends Controller
 {
-    function __construct($router) {
-        parent::__construct($router);
-
-        if (! $this->verify_session() || user()->is_admin != 0 && user()->is_admin != 1) {
-            $this->router->redirect('home');
-        }
-    }
-
     public function register($data)
     {
-        if (in_array('', $data)) {
-            $this->message = "Você precisa preencher todos os campos";
-            flask('response', $this->warningResponse());
-            $this->router->redirect('register');
-        }
-
-        $users = User::where('name', '=', $data['name'])->orWhere('email', '=', $data['email'])->get();
-
-        if ($users->count() > 0) {
-            $this->message = "Este usuário já existe";
-            flask('response', $this->warningResponse());
-            $this->router->redirect('register');
-        }
-
         $user = new User();
         $user->name = $data['name'];
         $user->username = $this->format_username($data['name']);
         $user->email = $data['email'];
-        $user->image = $data['image'];
-        $user->is_admin = $data['is_admin'];
-        $user->password = $this->encrypt_string('1234');
+        $user->password = $this->encrypt_string($data['password']);
 
         if ($user->save()) {
-            $this->message = "Usuário cadastrado";
-            flask('response', $this->successResponse());
-            $this->router->redirect("register");
+            $this->router->redirect("/");
         }
     }
 }
--- resources/views/modals.blade.php
+++ resources/views/modals.blade.php
@@ -1,12 +1,12 @@
 <!-- Login Modal -->
 <div class="modal fade" id="loginModal" tabindex="-1" aria-labelledby="loginModalLabel" aria-hidden="true">
-	<div class="modal-dialog modal-dialog-centered">
+	<form action="{{ $router->route('register') }}" method="post" class="modal-dialog modal-dialog-centered">
 		<div class="modal-content">
 			<div class="modal-header justify-content-center">
 				<h1 class="modal-title fs-5" id="loginModalLabel">Iniciar Sessão</h1>
 			</div>
 
-			<form class="modal-body">
+			<div class="modal-body">
 				<div class="mb-3">
 					<label class="col-12">
 						<span>E-mail</span>
@@ -20,60 +20,60 @@
 						<input type="password" class="form-control" />
 					</label>
 				</div>
-			</form>
+			</div>
 
 			<div class="modal-footer justify-content-center">
 				<button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fechar</button>
 				<button type="button" class="btn btn-primary">Salvar</button>
 			</div>
 		</div>
-	</div>
+	</form>
 </div>
 
 
 
 <!-- Register Modal -->
 <div class="modal fade" id="registerModal" tabindex="-1" aria-labelledby="registerModalLabel" aria-hidden="true">
-	<div class="modal-dialog modal-dialog-centered">
+	<form action="{{ $router->route('register') }}" method="post" class="modal-dialog modal-dialog-centered">
 		<div class="modal-content">
 			<div class="modal-header justify-content-center">
 				<h1 class="modal-title fs-5" id="registerModalLabel">Criar conta</h1>
 			</div>
 
-			<form action="{{ $router->route('register') }}" method="post" class="modal-body">
+			<div class="modal-body">
 				<div class="mb-3">
 					<label class="col-12">
 						<span>Nome</span>
-						<input type="text" class="form-control" />
+						<input type="text" name="name" class="form-control" />
 					</label>
 				</div>
 
 				<div class="mb-3">
 					<label class="col-12">
 						<span>E-mail</span>
-						<input type="text" class="form-control" />
+						<input type="text" name="email" class="form-control" />
 					</label>
 				</div>
 
 				<div class="mb-3">
 					<label class="col-12">
 						<span>Senha</span>
-						<input type="password" class="form-control" />
+						<input type="password" name="password" class="form-control" />
 					</label>
 				</div>
 
 				<div>
 					<label class="col-12">
 						<span>Confirme a senha</span>
-						<input type="password" class="form-control" />
+						<input type="password" name="confirm_password" class="form-control" />
 					</label>
 				</div>
-			</form>
+			</div>
 
 			<div class="modal-footer justify-content-center">
 				<button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fechar</button>
-				<button type="button" class="btn btn-primary">Salvar</button>
+				<button type="submit" class="btn btn-primary">Salvar</button>
 			</div>
 		</div>
-	</div>
+	</form>
 </div>
